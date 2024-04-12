3624aa231d23ce42e010e1cca5a0a1f7e8246f90
admin <1557156753@qq.com>
2024-04-13 00:29:58 +0800

    去掉运行allure报告

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pytest
 
 if __name__ == '__main__':
     pytest.main()
-    report_temp = r'testReport\report\temps'
-    report_dir = r'testReport\report\allure'
-    # os.system('allure generate' + report_temp + '-o' + report_dir + "--clean")
-    os.system("allure generate ./testReport/report/temps -o ./testReport/report/allures --clean")
+    # report_temp = r'testReport\report\temps'
+    # report_dir = r'testReport\report\allure'
+    # # os.system('allure generate' + report_temp + '-o' + report_dir + "--clean")
+    # os.system("allure generate ./testReport/report/temps -o ./testReport/report/allures --clean")
